bc2097c19b2e3e1f63c36c20cecc2abb8f470ad0
GuidoLorenzetti <guidobruera@gmail.com>
2023-11-20 02:09:34 -0300

    Terminado redes neuronales

---
+++ pruebagpu.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+print('tensorflow version: ', tf.__version__)
+
+x = [[3.]]
+y = [[4.]]
+print('Result: {}'.format(tf.matmul(x, y)))
+
+print(tf.config.list_physical_devices('GPU'))
